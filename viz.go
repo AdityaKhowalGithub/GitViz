cfda968739693ef8ce48b869f2cb385860038246
Aditya Khowal <adityakh2003@outlook.com>
2024-03-29 15:48:58 -0700

    init commit

---
+++ viz.go
@@ -0,0 +1,23 @@
+package main
+
+import ("flag")
+
+//takes in a path
+//crawls path and all subfolders
+//looks for git repos
+func scan(path string){
+
+}
+
+
+//generates git contribution graph
+func stats(email string){
+
+}
+
+
+
+func main(){
+
+
+}
